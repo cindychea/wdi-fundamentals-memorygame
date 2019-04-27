c391319fc29b8149d4c5353d69513a16e6f0252c
cindychea <cindy_chea@hotmail.com>
2019-04-26 23:39:02 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,15 +2,27 @@ console.log ("Up and running!");
 
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.")
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length % 2 === 0)
+		checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+//if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+//		alert("You found a match!");
+//	 else {
+//		alert("Sorry, try again.")
+//	}
