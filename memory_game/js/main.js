68953d14b3080ab53b79218822ea3c2784bbb8f0
cindychea <cindy_chea@hotmail.com>
2019-04-28 23:33:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,9 +32,11 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src',cards[cardId].cardImage)
 	if (cardsInPlay.length % 2 === 0) {
 		checkForMatch();
 	}
@@ -42,7 +44,16 @@ function flipCard(cardId) {
 	console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement)
+	}
+}
+
+createBoard();
 
 
